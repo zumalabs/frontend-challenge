cd69decaa3c47f68e466788fa4bee06d320568d5
Simon Day <simon.day91@hotmail.co.uk>
2021-04-28 02:16:31 +0800

    basic solve with text colour change

--- src/Components/Typescript/Tickable.tsx
+++ src/Components/Typescript/Tickable.tsx
@@ -1,11 +1,29 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
+import "./Tickable.css";
 
-interface Tickable { 
+// 🐸
+
+interface Tickable {
   value: string;
 }
 
-const TickableTS: FC<Tickable> = ({ value }) => { 
-  return null;
-}
+const TickableTS: FC<Tickable> = ({ value }) => {
+  const [preValue, setPrevValue] = useState<string>(value);
+  const [direction, setDirection] = useState<string>("");
+
+  useEffect(() => {
+    if (preValue > value) {
+      setDirection("down");
+    } else {
+      setDirection("up");
+    }
+  }, [value]);
+
+  return (
+    <div>
+      <h2 className={direction}>{value}</h2>
+    </div>
+  );
+};
 
 export default TickableTS;
