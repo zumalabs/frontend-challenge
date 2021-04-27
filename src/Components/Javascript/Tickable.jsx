ff1d6287b448970e0e9fe5fa2b36bb1bec1741a9
dariadoriani <daria.doriani@gmail.com>
2021-04-27 12:17:57 +0100

    Adding value difference absolut and percentage

--- src/Components/Javascript/Tickable.jsx
+++ src/Components/Javascript/Tickable.jsx
@@ -1,8 +1,14 @@
 const TickableJS = ({ value, previousValue }) => { 
+  const differenceInValue = value - previousValue;
+  const percentageDifference = Math.round(value / previousValue * 100) / 100;
+
+  const classToAdd = differenceInValue > 0 ? 'positive-ticker' : differenceInValue < 0 ? 'negative-ticker' : 'no-change-ticker';
+
   return (
     <div>
-      <p>{value}</p>
-      <p>{previousValue}</p>
+        <span> {value} </span>
+        <span> {differenceInValue} </span>
+        <span> {`(${percentageDifference}%)`} </span>
     </div>
   );
 }
