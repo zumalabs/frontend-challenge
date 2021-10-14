a0e9e552f5c1a0e68f9dee75cddb690870b6445c
KaSpiros <ka_spiros@yahoo.gr>
2021-10-14 19:09:08 +0100

    Implemented functionality to display value and appropriate colour on value change

--- src/Components/Javascript/Tickable.jsx
+++ src/Components/Javascript/Tickable.jsx
@@ -1,5 +1,25 @@
-const TickableJS = ({ value }) => { 
-  return null;
+import { useState } from "react";
+import useInterval from "../../Hooks/useInterval";
+
+const TickableJS = ({ value, tickSpeed }) => {
+  const [oldValue, setOldValue] = useState("0")
+
+  useInterval(() => {
+    setOldValue(value)
+  }, tickSpeed - 200)
+
+  const checkValue = () => { 
+    if( oldValue > value){
+    return <h1 style={{color:"red"}}>{value}</h1>
+  } if(oldValue < value) {
+    return <h1 style={{color:"green"}}>{value}</h1>
+  }else {
+    return <h1>{value}</h1>
+  }
+}
+
+  return <div>{checkValue()}</div>
+
 }
 
 export default TickableJS;
