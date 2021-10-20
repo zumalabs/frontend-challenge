ada19596a3332fa58f56bcef42695912d903125a
Manjoot <manjoot@hotmail.com>
2021-10-20 09:44:51 +0100

    feat: basic highlighting of increment and decrement

--- src/Components/Javascript/Tickable.jsx
+++ src/Components/Javascript/Tickable.jsx
@@ -4,10 +4,22 @@ const TickableJS = ({ value }) => {
 
   const previousValue = usePrevious(value)
 
+  const compareValues = () => {
+    if (value > previousValue) {
+      return <p style={{color: "green"}}>{value}</p>
+
+    } else if (previousValue > value) {
+      return <p style={{color: "red"}}>{value}</p>
+    } else {
+      return <p>{value}</p>
+    }
+  }
+
   return (
     <>
-      <p>Current Value: {value}</p>
-      <p>Previous Value: {previousValue}</p>
+      <p>Current Value:</p> {compareValues()}
+      <p>Previous Value: </p> 
+      <p>{previousValue}</p>
     </>
   );
 }
